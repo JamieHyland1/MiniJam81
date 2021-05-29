21181afc1174a30e242370c3d61f26bf6d3ee035
Jamie <Jamiehyland01@gmail.com>
2021-05-29 16:09:32 +0100

    EnemyAI

---
+++ Kaiju/Assets/Scripts/StateMachine/EnemySM/EnemyIdleState.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyIdleState : IState
+{
+    EnemySM _EnemySM;
+    Transform playerTransform;
+
+    public EnemyIdleState(EnemySM enemySM, Transform plyrTrans){
+        this._EnemySM = enemySM;
+        this.playerTransform = plyrTrans;
+    }
+    public void Enter(){
+        Debug.Log("CHANGED TO IDLE STATE");
+    }
+
+    public void Exit()
+    {
+    }
+
+    public void FixedTick()
+    {
+    }
+
+    public void Tick(){
+        if(Vector3.Distance(_EnemySM.transform.position,playerTransform.position) < 15f){
+            Debug.Log("Found player");
+            _EnemySM.ChangeState(_EnemySM.WalkState);
+        }
+
+
+    }
+}
---
+++ Kaiju/Assets/Scripts/StateMachine/EnemySM/EnemyShootState.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootState : IState
+{
+    EnemySM _EnemySM;
+    Transform PlayerTransform;
+
+    public EnemyShootState(EnemySM _esm, Transform plyrTrans){
+        this._EnemySM = _esm;
+        this.PlayerTransform = plyrTrans;
+    }
+    public void Enter(){
+        Debug.Log("ENTERING SHOOT STATE");
+    }
+    public void Exit(){
+        Debug.Log("LEAVING SHOOT STATE");
+
+    }
+    public void FixedTick(){
+
+    }
+    public void Tick(){
+        Debug.Log("BANG!");
+        _EnemySM.ChangeState(_EnemySM.IdleState);
+    }
+}
