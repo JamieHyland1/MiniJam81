1cf9b8d6cf0fc0eeba1811a49ba5e4dcb33594db
Jamie <Jamiehyland01@gmail.com>
2021-05-30 14:16:30 +0100

    Added dialogue code

---
+++ Kaiju/Assets/ListenForPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListenForPlayer : MonoBehaviour
+{
+   bool playerNearby = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerNearby && Input.GetMouseButtonDown(0)){
+            DialogueTrigger trig = this.GetComponent<DialogueTrigger>();
+            if(trig.dialoguePlaying){
+                FindObjectOfType<DialogueManager>().DisplayNextSentence();
+            }else{
+                trig.TriggerDialogue();
+            }
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision other) {
+        Debug.Log(other.gameObject.name);
+        if(other.gameObject.name =="Player"){
+            playerNearby = true;
+        }    
+    }
+
+    private void OnCollisionExit(Collision other) {
+         if(other.gameObject.name =="Player"){
+            playerNearby = true;
+        }   
+    }
+}
---
+++ Kaiju/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Queue<string> sentences;
+    public Text nameText;
+    public Text dialogueText;
+    public Animator animator;
+
+    public DialogueTrigger currentTrigger;
+
+
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue, DialogueTrigger trigger){
+        currentTrigger = trigger;
+        Debug.Log("");
+        animator.SetBool("isOpen",true);
+        nameText.text = dialogue.name;
+        sentences.Clear();
+        foreach(string sentence in dialogue.sentences){
+            sentences.Enqueue(sentence);
+        }
+
+    }
+    public void DisplayNextSentence(){
+        if(sentences.Count == 0){
+            endDialogue();
+            return;
+        }
+        dialogueText.text = sentences.Dequeue();
+    }
+
+    private void endDialogue(){
+        animator.SetBool("isOpen",false);
+        nameText.text = "";
+        dialogueText.text = "";
+        currentTrigger.dialoguePlaying = false;
+        currentTrigger = null;
+        sentences.Clear();
+    }
+}
